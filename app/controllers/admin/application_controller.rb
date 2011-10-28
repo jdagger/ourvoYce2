94eee506ef20526dd2b60e4258022eeacee48c5e
rmcalvert <ryan.m.calvert@gmail.com>
2011-10-28 17:03:53 -0400

    Added authentication check for admin pages

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -1,4 +1,12 @@
 class Admin::ApplicationController < ApplicationController
+  before_filter :check_access
+
   layout "admin"
 
+
+  def check_access
+    if current_user.nil?
+      redirect_to root_path
+    end
+  end
 end
